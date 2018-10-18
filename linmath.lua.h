e0e7a1ddbaab6535f11b1c1f0a08caa7c94905f2
Jeremy Burns <j@jaburns.net>
2018-10-18 09:13:40 -0600

    Added project structure

---
+++ linmath.lua.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "linmath.h/linmath.h"
+
+// TODO implement
---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "linmath.lua.h"
+
+int main (int argc, char **argv)
+{
+    // setup lua interpreter
+    // load linmath.lua
+    // run test.lua
+}
